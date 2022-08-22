e4988e2ce15c4f306c3faafe6a778236d61e5a4d
luyomo <syunka.tyo@gmail.com>
2022-08-22 23:53:17 +0900

    Test

--- BTMU_Helloworld/main.py
+++ BTMU_Helloworld/main.py
@@ -15,9 +15,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    logging.info(f"Tge yss path is {sys.path}")    
+    logging.info(f"Tge yss path is {sys.path}")
     if name:
-        return func.HttpResponse(f"Hello, {name}. This is the third hello world. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This is the forth hello world. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
