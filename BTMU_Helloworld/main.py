e6e4e0b4d83cd068ab1db3ecd5b3def9d89087b7
luyomo <syunka.tyo@gmail.com>
2022-08-22 23:51:05 +0900

    First push

---
+++ BTMU_Helloworld/main.py
@@ -0,0 +1,25 @@
+import logging
+import azure.functions as func
+#import pyodbc
+import sys
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    logging.info(f"Tge yss path is {sys.path}")    
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This is the third hello world. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
