2fc2f2ed3e3459bd3d0a54682b5eae1fb4fdbcfc
luyomo <syunka.tyo@gmail.com>
2022-08-24 21:02:15 +0900

    push change

--- BTMU_PKG/upload/readRowLIFEJ.py
+++ BTMU_PKG/upload/readRowLIFEJ.py
@@ -380,7 +380,7 @@ class readRowLIFEJ(Common):
       def dbExecute(cursor):
         try:
           for _idx, _line in enumerate(_data):
-            __query = f"insert into dxc.transbiz_row values('{_line['process_date']}', {_idx + 1} , '{_line['bank_code']}', '{_line['pay_date']}', '{_line['row_detail'].encode('sjis')}', '{_line['row_type']}', current_timestamp, current_user)"
+            __query = f"insert into dxc.transbiz_row values('{_line['process_date']}', {_idx + 1} , '{_line['bank_code']}', '{_line['pay_date']}', N'{_line['row_detail']}', '{_line['row_type']}', current_timestamp, current_user)"
             #__query = f"insert into dxc.transbiz_row values('{_line['process_date']}', {_idx + 1} , '{_line['bank_code']}', '{_line['pay_date']}', ?, '{_line['row_type']}', current_timestamp, current_user)"
             logging.info(f"the query is {__query}")
             cursor.execute(__query)
