933dfaf7b46228ee6ad13ef97a389e66e201deb4
luyomo <syunka.tyo@gmail.com>
2022-08-24 19:30:52 +0900

    Pushchanged

--- BTMU_PKG/upload/readRowLIFEJ.py
+++ BTMU_PKG/upload/readRowLIFEJ.py
@@ -380,7 +380,7 @@ class readRowLIFEJ(Common):
       def dbExecute(cursor):
         try:
           for _idx, _line in enumerate(_data):
-            __query = print(f"insert into dxc.transbiz_row values('{_line['process_date']}', {_idx + 1} , '{_line['bank_code']}', '{_line['pay_date']}', '{_line['row_detail']}', '{_line['row_type']}', current_timestamp, current_user)")
+            __query = f"insert into dxc.transbiz_row values('{_line['process_date']}', {_idx + 1} , '{_line['bank_code']}', '{_line['pay_date']}', '{_line['row_detail']}', '{_line['row_type']}', current_timestamp, current_user)"
             logging.info(__query)
             cursor.execute(__query)
         except pyodbc.Error as ex:
